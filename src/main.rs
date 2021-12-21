0404459e68d33922acd4222996b99632653c5e93
Luís Von Muller <luisvmuller@icloud.com>
2021-12-21 11:14:09 -0300

    How to: 417

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,8 @@ async fn main() {
          */
 
         /**
-         * !Atention -> I don't know the fuck I'm doing and this must be wrong.
-         * !ME -> Wen't to make cofe, just a sec
+         * !Atention -> I don't know the fuck I'm doing and this maybe (must) be wrong.
+         * * Strongly looks like a fucking memory leak. Even the Box calls Leak (ノಠ益ಠ)ノ彡┻━┻
          */
         let room_info: &'static str = Box::leak(
             request
@@ -75,16 +75,20 @@ async fn main() {
                 .unwrap()
                 .to_string()
                 .into_boxed_str(),
-        );
-        // Structures the HttpBody as Bytes.  - Dont know the diff betwen vec<u8> or Bytes ?
+        );  // !TODO: Rewrite this when I discovers how to.
+
         let body_chunks: Vec<Result<&str, std::io::Error>> = vec![
             Ok("Hey there, either you don't have permissions to access the room or if you does, this room named: '"),
-            Ok(room_info), // How to make this live life enough?
+            Ok(room_info), 
             Ok("' does not exist anymore."),
         ];
 
         let body_stream = futures_util::stream::iter(body_chunks);
 
+        let res = Response::builder()
+                .status(417) // Expectation Failed, as always
+                .body(Body::wrap_stream(body_stream))
+                .expect("Unable to create `http::Response`");
         /**
          * !Ideas:
          * * Looks like "The response" don't matters alot? - Maybe just send over the body?
@@ -93,13 +97,9 @@ async fn main() {
          *  * The Response Comes from -> Hyper
          *  * Tower holds the closure
         //  * */
-        // let res = Response::builder()
-        //     .status(417) // Expectation Failed, as always
-        //     .body(body)
-        //     .expect("Unable to create `http::Response`");
 
         //Ok::<_, Infallible>(Response::new(Body::empty())) // Why this works?
-        Ok::<_, Infallible>(Response::new(Body::wrap_stream(body_stream))) // And this not? Since the Type is the same?
+        Ok::<_, Infallible>(res) // And this not? Since the Type is the same?
     });
 
     // build our application with some routes
